017867e91b5b65552ff75909ecf11f2b8a800f75
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2021-07-25 22:45:41 +0800

    update repo from template

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <VueUseDemo />
+  <div>
+    <h1>FFXIV Macro Generator</h1>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import VueUseDemo from './components/VueUseDemo.vue'
+<script setup lang="ts">
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-    VueUseDemo,
-  }
-})
 </script>
 
 <style>
